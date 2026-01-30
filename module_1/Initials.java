8b1963c07383076ea9d9243a788b02344bcd7411
mini-moose <jaredt.west@gmail.com>
2026-01-29 22:14:29 -0500

    basic functionality

--- module_1/Initials.java
+++ module_1/Initials.java
@@ -2,7 +2,18 @@ public class Initials
 {
     public static void main (String[] args)
 {
-    System.out.println("An emergency Broadcast I created my second program");
-    System.out.println("by your name");
-}
+    Integer ROW_WIDTH = 9;
+    String letter = "000111000001111100011000110011000110011111110011111110011000110011000110011000110";
+        for (int i = 0; i < letter.length(); i++) {
+            char bit = letter.charAt(i);
+
+            char outputChar = (bit == '0') ? '-' : 'X';
+            System.out.print(outputChar);
+            System.out.print(" ");
+
+            if ((i + 1) % ROW_WIDTH == 0) {
+                System.out.println();
+            }
+        }
+    }
 }
