6778249e410adffd42028bf04720624f1e95da3e
mini-moose <jaredt.west@gmail.com>
2026-01-29 20:28:08 -0500

    initial commit

---
+++ module_1/Initials.java
@@ -0,0 +1,8 @@
+public class Initials
+{
+    public static void main (String[] args)
+{
+    System.out.println("An emergency Broadcast I created my second program");
+    System.out.println("by your name");
+}
+}
