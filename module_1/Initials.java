a1f4ed6d2d3cd88a0c06b8f0b5200806a23cc639
mini-moose <jaredt.west@gmail.com>
2026-02-11 18:40:32 -0500

    finished initals

--- module_1/Initials.java
+++ module_1/Initials.java
@@ -1,13 +1,23 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Initials
 {
     public static void main (String[] args)
 {
     Integer ROW_WIDTH = 9;
-    String letter = "000111000001111100011000110011000110011111110011111110011000110011000110011000110";
+    LinkedHashMap<String, String> letters = new LinkedHashMap<>();
+    letters.put("J","111111111111111111000011100000011100000011100000011100110011100111111000011100000");
+    letters.put("T","111111111111111111000111000000111000000111000000111000000111000000111000000111000");
+    letters.put("W","110000011110000011110010011110111011111111111011111110011111110011101110011000110");
+    for (Map.Entry<String, String> entry : letters.entrySet()) {
+        String letter = entry.getValue();
+        char letterChar = entry.getKey().charAt(0);
+        
         for (int i = 0; i < letter.length(); i++) {
             char bit = letter.charAt(i);
 
-            char outputChar = (bit == '0') ? '-' : 'X';
+            char outputChar = (bit == '0') ? '-' : letterChar;
             System.out.print(outputChar);
             System.out.print(" ");
 
@@ -15,5 +25,6 @@ public class Initials
                 System.out.println();
             }
         }
+    }
     }
 }
